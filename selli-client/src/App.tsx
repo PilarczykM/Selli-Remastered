8e5904966ad616527e24757e94ae6663ce9c2e62
Pilarczykm <marcinpilarczyk@me.com>
2020-05-24 15:19:23 +0200

    Adding router.

--- selli-client/src/App.tsx
+++ selli-client/src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { GlobalStyle } from './GlobalStyles';
+import { Home } from './modules/Home/Home';
+import { Register } from './modules/Register/Register';
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <h1>Hello World</h1>
+      <Router>
+      <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route path="/register" component={Register}/>
+          </Switch>
+      </Router>
     </>
   )
 }
