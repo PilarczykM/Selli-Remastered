1cb9330a38a7586ab129a9596b781a1783cf222a
spudso <65819767+spudso@users.noreply.github.com>
2020-05-24 18:09:42 +0200

    mr test

--- selli-client/src/modules/Home/Home.tsx
+++ selli-client/src/modules/Home/Home.tsx
@@ -1,3 +1,3 @@
-import * as React from 'react';
+import * as React from "react";
 
-export const Home: React.FC = () => (<h1>Home module</h1>)
+export const Home: React.FC = () => <h1>Home Module</h1>;
