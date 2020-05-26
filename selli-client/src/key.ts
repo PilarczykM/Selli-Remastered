41c0a0e7f6e51702a2b0021eb8d9c532a177218f
Marcin Pilarczyk <marcinpilarczyk@me.com>
2020-05-26 22:08:55 +0200

    remove keys from repo.

--- selli-client/src/key.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: 'AIzaSyDg8dNejME0B-bcChi9b107iQ8FbVKS1eU',
-  authDomain: 'selli-remastered.firebaseapp.com',
-  databaseURL: 'https://selli-remastered.firebaseio.com',
-  projectId: 'selli-remastered',
-  storageBucket: 'selli-remastered.appspot.com',
-  messagingSenderId: '580099165810',
-  appId: '1:580099165810:web:9905d98e295c63451b7ceb',
-}
